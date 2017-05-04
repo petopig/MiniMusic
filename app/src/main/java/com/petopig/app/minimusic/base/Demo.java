cbc98cb8bf761a7d510bba34f56b13b7b9b0e29c
petopig <petopig@163.com>
2017-05-04 17:02:43 +0800

    Rejected commit: Default

---
+++ app/src/main/java/com/petopig/app/minimusic/base/Demo.java
@@ -0,0 +1,13 @@
+package com.petopig.app.minimusic.base;
+
+/**
+ * Created by petopig on 2017/5/4.
+ */
+
+public class Demo {
+   public void Say(){
+       System.out.print("hdfdf");
+
+
+   }
+}
